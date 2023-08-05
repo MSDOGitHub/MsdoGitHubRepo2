b868ab727567c97ab5743d2e4780981aa1356f54
bijenderbisht <113984589+bijenderbisht@users.noreply.github.com>
2023-08-05 17:45:26 +0530

    Create dependency.js

---
+++ dependency.js
@@ -0,0 +1,12 @@
+{
+  "name": "npm-overrides",
+  "version": "1.0.0",
+  "license": "MIT",
+  "dependencies": {
+    "axios": "0.18.0"
+  },
+  "overrides": {
+    // BAD, will throw an EOVERRIDE error
+    // "axios": "0.19.2",
+  }
+}
